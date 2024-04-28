9610a901550aa8697134a5db28ecc85248385885
raul crespo <raulxcrespo@gmail.com>
2024-04-28 19:15:37 -0300

    desafio com exception

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = scan.nextInt();
+
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = scan.nextInt();
+        scan.close();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e);
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        } else {
+            int contagem = parametroDois - parametroUm;
+            for (int i = 1; i <= contagem; i++) {
+                System.out.println("Imprimindo o número " + i);
+            }
+        }
+
+    }
+}
